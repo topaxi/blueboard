e7fdc03dbc08eea00abf090bd3cceccbef76fdbd
Damian Senn <damian.senn@gmail.com>
2015-07-15 00:42:41 +0200

    Use tagName table for bb-board component

--- app/components/bb-board/component.js
+++ app/components/bb-board/component.js
@@ -3,6 +3,8 @@ import Ember from 'ember'
 const { computed } = Ember
 
 export default Ember.Component.extend({
+  tagName: 'table',
+
   hasPlatforms: computed('stationboard.@each.stop.platform', function() {
     return this.get('stationboard').some(i => i.stop.platform)
   })
